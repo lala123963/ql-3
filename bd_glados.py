d04c7fa9bce36f4487911961783af54cae6f0c6c
Bidepan <951150259@qq.com>
2023-04-05 21:14:11 +0800

    优化更新

--- bd_glados.py
+++ bd_glados.py
@@ -9,17 +9,19 @@ glados签到(免费梯子)
 多账号新建变量或者用 & 分开
 
 """
+
 import time
 import requests
 from os import environ, system, path
 
 
+# 读取通知
 def load_send():
     global send, mg
     cur_path = path.abspath(path.dirname(__file__))
-    if path.exists(cur_path + "/notify.py"):
+    if path.exists(cur_path + "/SendNotify.py"):
         try:
-            from notify import send
+            from SendNotify import send
             print("加载通知服务成功！")
         except:
             send = False
@@ -32,11 +34,12 @@ def load_send():
 load_send()
 
 
+# 获取环境变量
 def get_environ(key, default="", output=True):
     def no_read():
         if output:
-            if key == "gladosck":
-                print(f"未填写环境变量 {key} 请添加")
+            print(f"未填写环境变量 {key} 请添加")
+            exit(0)
         return default
 
     return environ.get(key) if environ.get(key) else no_read()
@@ -58,21 +61,41 @@ class Glados():
         data = {
             "token": "glados.network"
         }
-        r = requests.post(url, headers=headers, json=data).json()
-        xx = "[账号]{}\n[签到]{}\n\n".format(a, r['message'])
-        self.msg += xx
-        return self.msg
+        r = requests.post(url, headers=headers, json=data)
+        if r.status_code == 200:
+            if r.json()['message'] == '没有权限':
+                xx = f"[账号]{a}\n[签到]{r.json()['message']}，ck可能无效:{self.ck}\n\n"
+                self.msg += xx
+                return self.msg
+            else:
+                xx = f"[账号]{a}\n[签到]{r.json()['message']}\n\n"
+                self.msg += xx
+                return self.msg
+        else:
+            xx = f"[账号]{a}\n[签到]请检查网络或者ck有效性：{self.ck}\n\n"
+            self.msg += xx
+            return self.msg
+
+    def get_sign_msg(self):
+        return self.sign()
 
 
 if __name__ == '__main__':
-    token = get_environ("gladosck") if environ.get("gladosck") else refresh_token
+    token = get_environ("gladosck")
+    if not token:
+        print("未获取到gladosck环境变量，请检查配置")
+        exit(0)
     msg = ''
     cks = token.split("&")
-    print("检测到{}个ck记录".format(len(cks)))
-    print()
+    print("检测到{}个ck记录\n".format(len(cks)))
     a = 0
     for ck in cks:
         a += 1
         run = Glados(ck)
-        msg += run.sign()
-    send("glados签到通知", msg)
+        try:
+            msg += run.get_sign_msg()
+        except Exception as e:
+            print(f"第{a}个ck签到异常：{e}")
+            msg += f"[账号]{a}\n[签到]异常：{e}\n\n"
+    if send:
+        send("glados签到通知", msg)
